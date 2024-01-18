3c6518cafb6db3771b32053fd3ed6c820c260368
Kunal3337 <157002371+Kunal3337@users.noreply.github.com>
2024-01-18 12:33:25 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+// alert("What is your name") you can use this line only on browser console not on nodejs
+
+let name = "Kumar";
+let age = 18;
+let checkit = true;
+let temprature = null;
+let runs;
+
+//Primitive Datatypes
+/*
+1. Number => 2 to the power 53
+Also, a datatype "bigint" which is use large numbers and mostly used by giant like facebook.
+2. string => "Books". ....... we can also use string in single quote
+boolean = true or false
+Null => Stand alone Value
+undefined => this means we haven't assign anything
+symbol => It is used to show uniqueness
+*/
+
+//Object
+console.log(typeof undefined); // In Terminal the output will be undefined 
+console.log(typeof null); // In Terminal the output will be object
